8138ad91d46f1fae5afb90f8a382daf558e1dd5d
John Barnett <johnb@cadenceig.com>
2021-11-19 16:45:29 -0700

    Added ef core with sqlite and boilerplate for material.

---
+++ Entities/InvoiceLineItem.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Collections.Generic;
+
+namespace GroupProject.Entities
+{
+    public class InvoiceLineItem : IEntityTypeConfiguration<InvoiceLineItem>
+    {
+        public int InvoiceLineItemId { get; set; }
+        public int InvoiceId { get; set;}
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+        public Invoice Invoice { get; set; }
+
+        public void Configure(EntityTypeBuilder<InvoiceLineItem> builder)
+        {
+            builder.HasKey(x => x.InvoiceLineItemId);
+            builder.HasOne(x => x.Item)
+                .WithMany( x=>x.InvoiceLineItems);
+            builder.HasOne(x => x.Invoice)
+                .WithMany(x => x.InvoiceLineItems);
+        }
+    }
+}
---
+++ Entities/Item.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject.Entities
+{
+    public class Item : IEntityTypeConfiguration<Item>
+    {
+        public int ItemId { get; set; }
+        public string Description { get; set; }
+        public Decimal Cost { get; set; }
+
+        public IList<InvoiceLineItem> InvoiceLineItems { get; set; }
+        public void Configure(EntityTypeBuilder<Item> builder)
+        {
+            builder.HasKey(x => x.ItemId);
+            builder.Property(x => x.Cost)
+                .HasPrecision(19, 4);
+        }
+    }
+}
