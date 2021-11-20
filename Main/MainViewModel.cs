5751d93d5a986d9b6d83d84e697fca761caf9a69
John Barnett <johnb@cadenceig.com>
2021-11-20 11:51:43 -0700

    Updated the main view to match what was asked for the final project.

--- Main/MainViewModel.cs
+++ Main/MainViewModel.cs
@@ -9,8 +9,9 @@ namespace GroupProject.Main
     public class MainViewModel
     {
         // Will hold the logic behind the view and state for the view
-        // Handle navigation to search and 
+        // Handle Orchestrating what window is open on screen 
         // Database actions will be handled by the repository pattern possibly with sqlite depends on what the team decides
         // The MainWindow is the current window for this.
+        // Also we are using git to collaborate.
     }
 }
