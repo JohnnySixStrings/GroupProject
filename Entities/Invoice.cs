282badcd3330e870c5296d03f45f8ef8ec503cbe
John Barnett <johnb@cadenceig.com>
2021-11-20 11:45:04 -0700

    Added comments for all the entities

--- Entities/Invoice.cs
+++ Entities/Invoice.cs
@@ -7,12 +7,27 @@ namespace GroupProject.Entities
 {
     public class Invoice : IEntityTypeConfiguration<Invoice>
     {
+        /// <summary>
+        /// Invoice Key
+        /// </summary>
         public int InvoiceId { get; set; }
+        /// <summary>
+        /// User Set Invoice date
+        /// </summary>
         public DateTime InvoiceDate { get; set; }
+        /// <summary>
+        /// TotalCost
+        /// </summary>
         public Decimal TotalCost { get; set; }
-
+        /// <summary>
+        /// List of Related InvoiceLineItems
+        /// </summary>
         public IList<InvoiceLineItem> InvoiceLineItems { get; set; }
 
+        /// <summary>
+        /// builder for setting up properties and relationships
+        /// </summary>
+        /// <param name="builder"></param>
         public void Configure(EntityTypeBuilder<Invoice> builder)
         {
             builder.HasKey(x=> x.InvoiceId);
--- Entities/InvoiceLineItem.cs
+++ Entities/InvoiceLineItem.cs
@@ -6,12 +6,30 @@ namespace GroupProject.Entities
 {
     public class InvoiceLineItem : IEntityTypeConfiguration<InvoiceLineItem>
     {
+        /// <summary>
+        /// InvoiceLineItem Key
+        /// </summary>
         public int InvoiceLineItemId { get; set; }
+        /// <summary>
+        /// Invoice Foreign Key
+        /// </summary>
         public int InvoiceId { get; set;}
-        public int ItemId { get; set; }
+        /// <summary>
+        /// ItemDescription Foreign
+        /// </summary>
+        public int ItemDescriptionId { get; set; }
+        /// <summary>
+        /// ItemDescription Related to enity
+        /// </summary>
         public ItemDescription ItemDescription { get; set; }
+        /// <summary>
+        /// Invoice related to this entity
+        /// </summary>
         public Invoice Invoice { get; set; }
-
+        /// <summary>
+        /// for setting up relationships and properties of entity
+        /// </summary>
+        /// <param name="builder"></param>
         public void Configure(EntityTypeBuilder<InvoiceLineItem> builder)
         {
             builder.HasKey(x => x.InvoiceLineItemId);
