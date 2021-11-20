b6acd02d7486a9795ce743c2448eeccc8435cf68
John Barnett <johnb@cadenceig.com>
2021-11-20 11:50:56 -0700

    Added Comments to help describe usage or repo and added comments to view model.

--- Main/MainViewModel.cs
+++ Main/MainViewModel.cs
@@ -8,5 +8,9 @@ namespace GroupProject.Main
 {
     public class MainViewModel
     {
+        // Will hold the logic behind the view and state for the view
+        // Handle navigation to search and 
+        // Database actions will be handled by the repository pattern possibly with sqlite depends on what the team decides
+        // The MainWindow is the current window for this.
     }
 }
--- Repositories/InvoiceRepository.cs
+++ Repositories/InvoiceRepository.cs
@@ -11,6 +11,9 @@ namespace GroupProject.Repositories
         // we can use a shared repository to access the data shared by the application
         // sharing code instead of repeating the logic throughout the project using the repository pattern
         private readonly InvoiceDbContext _invoiceDb;
+        /// <summary>
+        /// Setups the DbContext to be used for the life of the repository.
+        /// </summary>
         public InvoiceRepository()
         {
             _invoiceDb = new InvoiceDbContext();
@@ -23,19 +26,29 @@ namespace GroupProject.Repositories
             _invoiceDb.Update(invoice);
             _invoiceDb.SaveChanges();
         }
-
+        /// <summary>
+        /// Deletes the passed in invoice from the database
+        /// </summary>
+        /// <param name="invoice"></param>
         public void DeleteInvoice(Invoice invoice)
         {
             _invoiceDb.Remove(invoice);
             _invoiceDb.SaveChanges();
         }
-
+        /// <summary>
+        /// Inserts invoice into the database
+        /// </summary>
+        /// <param name="invoice"></param>
         public void AddInvoice(Invoice invoice)
         {
             _invoiceDb.Add(invoice);
             _invoiceDb.SaveChanges();
         }
-
+        /// <summary>
+        /// returns an invoice by it is id if exists if it does not then returns null for you to handle
+        /// </summary>
+        /// <param name="invoiceId"></param>
+        /// <returns></returns>
         public Invoice? GetInvoive(int invoiceId)
         {
             var invoice = _invoiceDb.Invoices
@@ -47,6 +60,10 @@ namespace GroupProject.Repositories
             return invoice;
         }
 
+        /// <summary>
+        /// returns a list of all invoices in the database
+        /// </summary>
+        /// <returns></returns>
         public IList<Invoice> GetAllInvoices()
         {
             var invoices = _invoiceDb.Invoices
@@ -59,6 +76,10 @@ namespace GroupProject.Repositories
 
         //operations on lineItems
         #region
+        /// <summary>
+        /// Deletes a list of lineitems from the database
+        /// </summary>
+        /// <param name="invoiceId"></param>
         public void DeleteLineItems(int invoiceId)
         {
             var lineItems = _invoiceDb.InvoiceLineItems
@@ -68,12 +89,19 @@ namespace GroupProject.Repositories
             _invoiceDb.SaveChanges();
         }
 
+        /// <summary>
+        /// Deletes line Item from the database
+        /// </summary>
+        /// <param name="lineItems"></param>
         public void DeleteLineItems(IList<InvoiceLineItem> lineItems)
         {
             _invoiceDb.RemoveRange(lineItems);
             _invoiceDb.SaveChanges();
         }
-
+        /// <summary>
+        /// Inserts lineItem to the Database
+        /// </summary>
+        /// <param name="lineItem"></param>
         public void AddLineItem(InvoiceLineItem lineItem)
         {
             _invoiceDb.Add(lineItem);
@@ -83,24 +111,39 @@ namespace GroupProject.Repositories
 
         //operations on items
         #region
+        /// <summary>
+        /// Deletes passed in item from the database
+        /// </summary>
+        /// <param name="item"></param>
         public void DeleteItem(ItemDescription item)
         {
             _invoiceDb.Remove(item);
             _invoiceDb.SaveChanges();
         }
-        
+        /// <summary>
+        /// Inserts passed in item to the database
+        /// </summary>
+        /// <param name="item"></param>
         public void AddItem(ItemDescription item)
         {
             _invoiceDb.Add(item);
             _invoiceDb.SaveChanges();
         }
 
+        /// <summary>
+        /// Updates passed in item in the databasee
+        /// </summary>
+        /// <param name="item"></param>
         public void UpdateItem(ItemDescription item)
         {
             _invoiceDb.Update(item);
             _invoiceDb.SaveChanges();
         }
         
+        /// <summary>
+        /// Gets a list of all items in the database
+        /// </summary>
+        /// <returns></returns>
         public IList<ItemDescription> GetAllItems()
         {
             var items = _invoiceDb.ItemDescriptions
